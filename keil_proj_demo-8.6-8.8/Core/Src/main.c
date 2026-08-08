05c09e808e21d60f5c090bae7d48f4e027dba4af
wsl1006 <1528167029@qq.com>
2026-08-08 09:50:06 +0800

    改配置

--- keil_proj_demo-8.6-8.8/Core/Src/main.c
+++ keil_proj_demo-8.6-8.8/Core/Src/main.c
@@ -38,7 +38,7 @@
 #include "buzzer.h"   /* 蜂鸣器驱动的函数声明 */
 #include "led.h"      /* LED 驱动的函数声明 */
 
-#include "user_beep.h"；
+#include "user_beep.h"
 
 /* USER CODE END Includes */
 
@@ -52,7 +52,7 @@
 /* #define 是宏定义：编译前会把名字替换成后面的内容 */
 #define LED_COUNT   4U    /* 板载 LED 数量，U 表示 unsigned 无符号数 */
 #define BLINK_TIMES 5U    /* 每颗 LED 闪烁次数 */
-#define DELAY_MS    500U  /* LED 亮/灭持续时间，单位毫秒 */
+#define DELAY_MS    250U  /* LED 亮/灭持续时间，单位毫秒 */
 #define BEEP_MS     120U  /* 蜂鸣器响一声的时长 */
 /* USER CODE END PD */
 
